c4564d50d42c5e661511148255d27928989ac9a5
KostaProsenikov <k.prosenikov@blubito.com>
2019-04-16 10:13:34 +0300

    Small update on comments

--- file.ts
+++ file.ts
@@ -1,6 +1,7 @@
 import * as _ from 'lodash';
 
-// FIND MAX Object by PROP START
+// ------------------------------------------
+// Find Min And Max Property START
 
 const objects = [
     {name: 'beer', price: 10 }, 
@@ -15,10 +16,13 @@ const result2  = _.maxBy(objects, 'price');
 console.log('Min price object', result);
 console.log('Max price object', result2);
 
-// FIND MAX Object by PROP END
+// ------------------------------------------
+// Find Min And Max Property END
+// ------------------------------------------
 
-
+// ------------------------------------------
 // Split array into equal parts START
+// ------------------------------------------
 
 const longArray = [1, 3 , 5, 6, 7, 10];
 
@@ -31,9 +35,13 @@ const splitArraysByThree = _.chunk(longArray, 3);
 console.log('split array', splitArrayByTwo);
 console.log('split array', splitArraysByThree);
 
+// ------------------------------------------
 // Split array into equal parts END
+// ------------------------------------------
 
+// ------------------------------------------
 // Fill in array with values START
+// ------------------------------------------
 
 const arrWithNums = [2, 4, 6, 8];
 // Arguments
@@ -45,10 +53,13 @@ const newArr = _.fill(arrWithNums, 5, 1, 3);
 
 console.log('fill array values', newArr);
 
+// ------------------------------------------
 // Fill in array with values END
+// ------------------------------------------
 
-
+// ------------------------------------------
 // Sorting array of objects by property START
+// ------------------------------------------
 
 const movies = [
     { name: 'Avengers',   year: 2015 },
@@ -65,4 +76,7 @@ const sortedArr =  _.orderBy(movies, ['year', 'name'], ['asc', 'desc']);
 
 console.log('sortedArr', sortedArr);
 
+// ------------------------------------------
 // Sorting array of objects by property END
+// ------------------------------------------
+
